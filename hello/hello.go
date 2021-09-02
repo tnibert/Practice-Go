a27ad3b7a17cb0a511bb98792161d747462eb747
Tim Nibert <docz2a@gmail.com>
2021-09-02 22:50:59 +1000

    for loop

--- hello/hello.go
+++ hello/hello.go
@@ -23,4 +23,23 @@ func main() {
     for key, val := range messagemap {
         fmt.Printf("Key: %d, Value: %s\n", key, val)
     }
+
+    fmt.Println("Testing for loop")
+    res, err := forloop_sum(10)
+    fmt.Printf("sum: %d\n", res)
 }
+
+func forloop_sum(iters int) (int, error) {
+    sum := 0
+    for i:= 0; i < iters; i++ {
+        sum += i
+    }
+    return sum, nil
+}
+
+// todo - recursion
+//fun reduce_sum(iters int) (int) {
+//    if basecase
+//       return
+//    return recurse
+//}
