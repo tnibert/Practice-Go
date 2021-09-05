d829a5f4750f1ce3a5f0f6208471816b766597bb
Tim Nibert <docz2a@gmail.com>
2021-09-05 21:47:10 +1000

    applied gofmt

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,60 +1,60 @@
 package greetings
 
 import (
-    "fmt"
-    "errors"
-    "math/rand"
-    "time"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
 func Hello(name string, more string) (string, error) {
-    // If no name was given, return an error with a message.
-    if name == "" {
-        return "You deny me your name!?", errors.New("empty name")
-    }
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "You deny me your name!?", errors.New("empty name")
+	}
 
-    // Return a greeting that embeds the name in a message.
-    // if only one %v, we will have a message saying extra string
-    message := fmt.Sprintf(randomFormat(), name, more)
-    return message, nil
+	// Return a greeting that embeds the name in a message.
+	// if only one %v, we will have a message saying extra string
+	message := fmt.Sprintf(randomFormat(), name, more)
+	return message, nil
 }
 
 // Hellos returns a map that associates each of the named people
 // with a greeting message.
 func Hellos(names []string) (map[string]string, error) {
-    // A map to associate names with messages.
-    messages := make(map[string]string)
-    // Loop through the received slice of names, calling
-    // the Hello function to get a message for each name.
-    for _, name := range names {
-        message, err := Hello(name, "test")
-        if err != nil {
-            return nil, err
-        }
-        // In the map, associate the retrieved message with
-        // the name.
-        messages[name] = message
-    }
-    return messages, nil
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name, "test")
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // init sets initial values for variables used in the function.
 func init() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 // randomFormat returns one of a set of greeting messages. The returned
 // message is selected at random.
 func randomFormat() string {
-    // A slice of message formats.
-    formats := []string{
-        "Hi, %v.  Welcome!  This is your second parameter - %v",
-        "Great to see you, %v!",
-        "Hail, %v! Well met!",
-    }
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v.  Welcome!  This is your second parameter - %v",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
 
-    // Return a randomly selected message format by specifying
-    // a random index for the slice of formats.
-    return formats[rand.Intn(len(formats))]
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -1,110 +1,110 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "math"
-    "time"
-    "rsc.io/quote"
-    "ironlotuscomputer.com.au/greetings"
+	"fmt"
+	"ironlotuscomputer.com.au/greetings"
+	"log"
+	"math"
+	"rsc.io/quote"
+	"time"
 )
 
 /*
 This has just become a very long series of experiments
 */
 func main() {
-    // quote from quote module
-    fmt.Println(quote.Go())
+	// quote from quote module
+	fmt.Println(quote.Go())
 
-    // A slice of names.
-    names := []string{"Isis", "Ra", "Darrin"}
+	// A slice of names.
+	names := []string{"Isis", "Ra", "Darrin"}
 
-    // greeting from my module
-    messagemap, err := greetings.Hellos(names)
-    if err != nil {
-       log.Fatal(err)
-    }
+	// greeting from my module
+	messagemap, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for key, val := range messagemap {
-        fmt.Printf("Key: %d, Value: %s\n", key, val)
-    }
+	for key, val := range messagemap {
+		fmt.Printf("Key: %d, Value: %s\n", key, val)
+	}
 
-    fmt.Println("Testing for loop")
-    res, err := forloop_sum(2)
-    fmt.Printf("sum: %d\n", res)
+	fmt.Println("Testing for loop")
+	res, err := forloop_sum(2)
+	fmt.Printf("sum: %d\n", res)
 
-    s := []int{1, 2, 3, 4}
-    res = reduce_sum(s)
-    fmt.Printf("reduced: %d\n", res)
+	s := []int{1, 2, 3, 4}
+	res = reduce_sum(s)
+	fmt.Printf("reduced: %d\n", res)
 
-    // interesting
-    num := 3
-    for {
-        if num < 1 {
-            break
-        } else {
-            num--
-        }
-    }
-    fmt.Printf("the final number is: %d\n", num)
+	// interesting
+	num := 3
+	for {
+		if num < 1 {
+			break
+		} else {
+			num--
+		}
+	}
+	fmt.Printf("the final number is: %d\n", num)
 
-    // also interesting
-    if x := 6; x < 5 {
-        fmt.Printf("pre if statement set x to %d\n", x)
-    } else {
-        fmt.Printf("pre if statement variable declaration available in else: %d\n", x)
-    }
+	// also interesting
+	if x := 6; x < 5 {
+		fmt.Printf("pre if statement set x to %d\n", x)
+	} else {
+		fmt.Printf("pre if statement variable declaration available in else: %d\n", x)
+	}
 
-    // square root algorithm
-    fmt.Println(Sqrt(9))
+	// square root algorithm
+	fmt.Println(Sqrt(9))
 
-    // switch with no condition is like switch true
-    t := time.Now()
-    switch {
-    case t.Hour() < 12:
-         fmt.Println("Good morning!")
-    case t.Hour() < 17:
-         fmt.Println("Good afternoon.")
-    default:
-        fmt.Println("Good evening.")
-    }
+	// switch with no condition is like switch true
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
 
-    // test defer
-    defer_helloworld()
+	// test defer
+	defer_helloworld()
 }
 
 func forloop_sum(iters int) (int, error) {
-    sum := 0
-    for i:= 0; i < iters; i++ {
-        sum += i
-    }
-    return sum, nil
+	sum := 0
+	for i := 0; i < iters; i++ {
+		sum += i
+	}
+	return sum, nil
 }
 
 // reduce a slice
-func reduce_sum(myslice []int) (int) {
-    if len(myslice) == 1 {
-        return myslice[0]
-    }
-    return myslice[len(myslice)-1] + reduce_sum(myslice[:len(myslice)-1])
+func reduce_sum(myslice []int) int {
+	if len(myslice) == 1 {
+		return myslice[0]
+	}
+	return myslice[len(myslice)-1] + reduce_sum(myslice[:len(myslice)-1])
 }
 
 func Sqrt(x float64) float64 {
-    guess := 1.0
-    precision := math.Pow(10, -10)
+	guess := 1.0
+	precision := math.Pow(10, -10)
 
-    fmt.Println("Computing square root")
-    for math.Abs(guess * guess - x) > precision {
-        guess -= (guess*guess - x) / (2*guess)
-        fmt.Printf("Guess: %.10f\n", guess)
-    }
-    return guess
+	fmt.Println("Computing square root")
+	for math.Abs(guess*guess-x) > precision {
+		guess -= (guess*guess - x) / (2 * guess)
+		fmt.Printf("Guess: %.10f\n", guess)
+	}
+	return guess
 }
 
 func defer_helloworld() {
-     // world will print when function returns
-     defer fmt.Println("world")
-     // apparently defer executes in reverse order
-     defer fmt.Println("When is this one executed?")
-     fmt.Println("hello")
+	// world will print when function returns
+	defer fmt.Println("world")
+	// apparently defer executes in reverse order
+	defer fmt.Println("When is this one executed?")
+	fmt.Println("hello")
 }
